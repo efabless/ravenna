a1375c34a2504fed13ad46e26da3bb28777d97fb
jeffdi <jeffdi@efabless.com>
2020-02-09 21:41:02 -0800

    uart - flush output

--- firmware/uart/uart.c
+++ firmware/uart/uart.c
@@ -249,11 +249,8 @@ void main()
     for(count=0;;count++) {
 		reg_gpio_data = (count >> 16);
         // for (j = 0; j < 17000; j++); // 2 sec
-        if ((count && 0x000f) == 0)
-        {
-            print_hex(count, 4);
-            print("\n");
-        }
+        print_hex(count, 4);
+        print("\n");
     }
 }
 
