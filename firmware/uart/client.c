4d940b7854561641067cdc06fd6efebd6177703b
jeffdi <jeffdi@efabless.com>
2020-02-24 10:20:14 -0800

    uart - debug

--- firmware/uart/client.c
+++ firmware/uart/client.c
@@ -101,10 +101,10 @@ char getch()
     int r;
     unsigned char c;
     if ((r = read(0, &c, sizeof(c))) < 0) {
-        print('1');
+        printf('1');
         return r;
     } else {
-        print('0');
+        printf('0');
         return c;
     }
 }
