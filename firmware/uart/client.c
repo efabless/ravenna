803e6e56233e473bef773371ff3c062a547087ae
jeffdi <jeffdi@efabless.com>
2020-02-26 15:01:44 -0800

    uart - debug

--- firmware/uart/client.c
+++ firmware/uart/client.c
@@ -149,7 +149,7 @@ int main()
                     }
 
                     putchar(buf[i++]);
-                    printf("%04x", c);
+                    printf("%04x\n", c);
                 }
                 fflush(stdout);
             }
