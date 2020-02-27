f18aa5bd577145f4608cdcf3e04a488bc68e4705
jeffdi <jeffdi@efabless.com>
2020-02-26 17:09:10 -0800

    uart - debug

--- firmware/uart/uart.c
+++ firmware/uart/uart.c
@@ -232,7 +232,8 @@ void main()
 	// So at this crystal rate, use clkdiv = 6667 for 9600 baud.
 
 	// Set UART clock to 9600 baud
-	reg_uart_clkdiv = 6668;
+//	reg_uart_clkdiv = 6667;
+	reg_uart_clkdiv = 6670;
 
 	reg_gpio_enb = 0x0000;
 	reg_gpio_data = 0x0001;
